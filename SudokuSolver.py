e86456ba7d0a9a61c774ad49abbc23fd9bcad94d
ines-lorquet <ines.lorquet@laplateforme.io>
2024-03-18 17:06:53 +0100

    Création de la méthode random_replace pour remplacer les cases vide

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -3,3 +3,9 @@ import random
 class SudokuSolver:
     def __init__(self):
         pass
+    # Replace the underscores with random numbers
+    def random_replace(self, sudoku):
+        for i in range(9):
+            for j in range(9):
+                if sudoku[i][j] == '_':
+                    sudoku[i][j] = str(random.randint(1, 9))
